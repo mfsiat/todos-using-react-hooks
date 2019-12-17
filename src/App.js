072ef9bebf0baffe8fb8796c12828659ce57eefb
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-12-17 13:39:02 +0600

    added todo update which updates the todos

--- src/App.js
+++ src/App.js
@@ -10,6 +10,31 @@ function Todo({ todo, index }) {
   )
 }
 
+// it uses the sueState hook 
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState('');
+
+
+  // it handles the form submission which first takes the event pera-meter  
+  const handleSubmit = e => {
+    e.preventDefault(); //  make sure prevent the default actions 
+    if(!value) return; // will not submit for an empty value 
+    addTodo(value); // takes the inputted value 
+    setValue(''); // after submitting the todo the input becomes empty for taking new todo 
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text" 
+        className="input" 
+        value={value} 
+        placeholder="Add Todo..."
+        onChange={e => setValue(e.target.value)}/> 
+    </form>
+  )
+}
+
 function App(){
   const [todos, setTodos] = useState([
     {
@@ -26,12 +51,21 @@ function App(){
     },
   ]);
 
+  // we are updating the state 
+  // created newTodo which takes todo and adds on text 
+  // then uses setTodos as the new todo 
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
           <Todo key={index} index={index} todo={todo} />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   )
