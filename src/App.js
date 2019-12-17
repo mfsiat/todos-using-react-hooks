7dac3819ad4331882af7ebf9fbb203ea057fcd7e
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-12-17 13:53:46 +0600

    added button

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React, { useState } from 'react';
 import './App.css';
 
 
-function Todo({ todo, index }) {
+function Todo({ todo, index, completeTodo }) {
   return (
-    <div className="todo">
+    <div 
+      style={{textDecoration: todo.isCompleted ? 'line-through' : ''}}
+      className="todo">
       { todo.text }
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+      </div>
     </div>
   )
 }
@@ -59,11 +64,22 @@ function App(){
     setTodos(newTodos);
   };
 
+  // constant for the todo completion 
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  }
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo 
+            key={index} 
+            index={index} 
+            todo={todo}
+            completeTodo={completeTodo} />
         ))}
         <TodoForm addTodo={addTodo} />
       </div>
